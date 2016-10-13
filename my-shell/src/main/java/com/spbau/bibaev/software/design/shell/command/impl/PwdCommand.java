8262edd784c7aaac16815a52ee0e33991fbda270
Vitaliy Bibaev <roenke54@gmail.com>
2016-10-13 11:03:27 +0300

    Fix pwd command (use current directory from Environment)

--- my-shell/src/main/java/com/spbau/bibaev/software/design/shell/command/impl/PwdCommand.java
+++ my-shell/src/main/java/com/spbau/bibaev/software/design/shell/command/impl/PwdCommand.java
@@ -1,10 +1,14 @@
 package com.spbau.bibaev.software.design.shell.command.impl;
 
-import com.spbau.bibaev.software.design.shell.command.ExecutionResult;
+import com.spbau.bibaev.software.design.shell.EnvironmentImpl;
 import com.spbau.bibaev.software.design.shell.command.CommandBase;
+import com.spbau.bibaev.software.design.shell.command.ExecutionResult;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Path;
 import java.util.List;
 
 /**
@@ -27,8 +31,10 @@ public class PwdCommand extends CommandBase {
 
   @NotNull
   @Override
-  public ExecutionResult execute(@NotNull InputStream in, @NotNull OutputStream out, @NotNull OutputStream err) throws IOException {
-    out.write(System.getProperty("user.dir").getBytes());
+  public ExecutionResult execute(@NotNull InputStream in, @NotNull OutputStream out, @NotNull OutputStream err)
+      throws IOException {
+    Path currentDirectory = EnvironmentImpl.getInstance().getCurrentDirectory();
+    out.write(currentDirectory.toAbsolutePath().toString().getBytes());
     out.write(System.lineSeparator().getBytes());
     return ExecutionResult.CONTINUE;
   }
